63e7e5dabfe4795b923d739ff4932dab9317ef8c
orelpa <pa.orel@yandex.ru>
2024-02-15 09:58:22 +0300

    Перенос обработыки нажатий в функцию

---
+++ game_functions.py
@@ -0,0 +1,8 @@
+import pygame
+import sys
+
+def check_events():
+    """Обрабатывает нажатия клавиш и события мыши"""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
