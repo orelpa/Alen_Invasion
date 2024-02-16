4a22e7f85532b07a7e6add5a0d17ce5b1fb3cd80
orelpa <pa.orel@yandex.ru>
2024-02-16 11:20:48 +0300

    добавлен параметр ai_settings для создания экземпляра класса ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -20,7 +20,7 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
     
     #создание корабля
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
     
     
     #назначение цвета фона
