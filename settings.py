965d91c36875988e9dfc89b3f81caeb1534d5ec1
orelpa <pa.orel@yandex.ru>
2024-02-16 11:21:12 +0300

    добавлена переменная self.ship_speed_factor = 1.5 скорость движения корабля

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ class Settings():
         """Инициализирует настройки игры"""
         
         #Параметры экрана
-
+        self.ship_speed_factor = 1.5
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (77, 143, 172)
