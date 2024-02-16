b4f35d6aaf8efb5a1b5d6b2fb06f056b936389ff
orelpa <pa.orel@yandex.ru>
2024-02-16 11:21:32 +0300

    изменение логики движения корабля

--- ship.py
+++ ship.py
@@ -1,10 +1,14 @@
 import pygame
 
 class Ship():
-    def __init__(self, screen):
+    #настройка корабля
+
+    def __init__(self, ai_settings, screen):
         """Инициализирует корабль и задает его начальную позициб"""
 
         self.screen = screen
+        self.ai_settings = ai_settings
+
 
         #Загрузка изображения корабля и получение прямоугольника
         self.image = pygame.image.load('images/galactica.png')
@@ -15,16 +19,23 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx    
         self.rect.bottom = self.screen_rect.bottom
 
+        #сохранение вещественной координаты центра коробля
+        self.center = float(self.rect.centerx)
+
         #Флаг перемещение
         self.moving_right = False
         self.moving_left = False
-    
+
+    ship_speed_factor = 1.5
     def update(self):
         """Обновляет позицию корабля с учетом флага"""
+            #обновляется атрибут center, не rect
         if self.moving_right:
-            self.rect.centerx += 1
+            self.center += self.ai_settings.ship_speed_factor
         if self.moving_left:
-            self.rect.centerx -= 1
+            self.center -= self.ai_settings.ship_speed_factor
+        #обновление атрибута rect а основании self.center
+        self.rect.centerx = self.center
 
     def blitme(self):
         """Рисует корабльь в текущей позиции"""
